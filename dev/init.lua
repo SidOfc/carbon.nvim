f2fc3c7dc45bb66cb8a8c4cfe2ecef4e04171ea6
Sidney Liebrand <sidneyliebrand@gmail.com>
2022-01-02 14:38:11 +0100

    use absolute path for plugin rtp entry to use it while not in plugin directory (e.g. for testing home dir)

--- dev/init.lua
+++ dev/init.lua
@@ -1,3 +1,3 @@
-vim.opt.runtimepath:append({ vim.fn.getcwd() })
+vim.opt.runtimepath:append({ vim.env.HOME .. '/Dev/sidofc/lua/carbon.nvim' })
 vim.opt.runtimepath:remove({ vim.env.HOME .. '/.config/nvim' })
 vim.opt.packpath:remove({ vim.env.HOME .. '/.local/share/nvim/site' })
