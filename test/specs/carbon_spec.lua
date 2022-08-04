f9ff8531e57e68d994d44d93d8c6ad6cfa890e43
Sidney Liebrand <sidneyliebrand@gmail.com>
2022-08-04 18:41:28 +0200

    add keymap specs to specs/carbon_spec.lua

--- test/specs/carbon_spec.lua
+++ test/specs/carbon_spec.lua
@@ -1,5 +1,17 @@
+local ns = require('carbon.ns')
 local util = require('carbon.util')
 local settings = require('carbon.settings')
+
+local function cwd_dirname()
+  return vim.fn.fnamemodify(vim.loop.cwd(), ':t') .. '/'
+end
+
+local function get_carbon_autocmd(event)
+  return vim.api.nvim_get_autocmds({
+    group = ns.augroup,
+    event = event,
+  })[1] or {}
+end
 
 describe('carbon', function()
   it('opens a buffer with name "carbon"', function()
@@ -12,8 +24,8 @@ describe('carbon', function()
   end)
 
   it('displays the contents of the current directory', function()
-    assert.same(vim.api.nvim_buf_get_lines(0, 0, -1, true), {
-      vim.fn.fnamemodify(vim.loop.cwd(), ':t') .. '/',
+    assert.same({
+      cwd_dirname(),
       '  dev/init.lua',
       '+ doc/',
       '+ lua/',
@@ -24,20 +36,46 @@ describe('carbon', function()
       '  Makefile',
       '  README.md',
       '  stylua.toml',
-    })
+    }, vim.api.nvim_buf_get_lines(0, 0, -1, true))
   end)
 
-  for action, key in pairs(settings.actions) do
-    local plug = util.plug(action)
-    local plug_maparg = string.lower(vim.fn.maparg(util.plug(action), 'n'))
-    local key_maparg = string.lower(vim.fn.maparg(key, 'n'))
+  describe('default autocommands', function()
+    describe('DirChanged', function()
+      local autocmd = get_carbon_autocmd('DirChanged')
 
-    it(string.format('maps %s to carbon.%s', plug, action), function()
-      assert.is_number(string.find(plug_maparg, '<lua %w+'))
-    end)
+      it('exists', function()
+        assert.is_number(autocmd.id)
+      end)
 
-    it(string.format('maps %s to %s', key, plug), function()
-      assert.same(key_maparg, plug)
+      it('not buffer local', function()
+        assert.is_false(autocmd.buflocal)
+      end)
+
+      it('pattern is global', function()
+        assert.is_same('global', autocmd.pattern)
+      end)
     end)
-  end
+  end)
+
+  describe('plug mappings', function()
+    for action in pairs(settings.actions) do
+      local plug = util.plug(action)
+
+      it(string.format('maps %s to carbon.%s()', plug, action), function()
+        assert.is_number(
+          string.find(string.lower(vim.fn.maparg(plug, 'n')), '<lua %w+')
+        )
+      end)
+    end
+  end)
+
+  describe('action mappings', function()
+    for action, key in pairs(settings.actions) do
+      local plug = util.plug(action)
+
+      it(string.format('maps %s to %s', key, plug), function()
+        assert.same(string.lower(vim.fn.maparg(key, 'n')), plug)
+      end)
+    end
+  end)
 end)
