be5b3095fcfb08af29a851db8411a03270ae0fb5
Sidney Liebrand <sidneyliebrand@gmail.com>
2022-08-08 02:18:48 +0200

    patch/settings/keep netrw (#78)
    * Improvement: documentation of `settings.keep_netrw`
    
    * `settings.keep_netrw` now also clears NetRW augroups `FileExplorer` and `Network`
    
    * Add: tests for `settings.keep_netrw` behavior

--- lua/carbon.lua
+++ lua/carbon.lua
@@ -52,6 +52,9 @@ function carbon.initialize()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
+    pcall(vim.api.nvim_del_augroup_by_name, 'FileExplorer')
+    pcall(vim.api.nvim_del_augroup_by_name, 'Network')
+
     util.command('Explore', carbon.explore, { bang = true })
     util.command('Lexplore', carbon.explore_left, { bang = true })
   end
