a4a6bd547401c2c336b0ba4d4fc96e519e01c5ab
Sidney Liebrand <sidneyliebrand@gmail.com>
2024-03-18 13:00:25 +0100

    Fix healthcheck deprecations

--- lua/carbon/health.lua
+++ lua/carbon/health.lua
@@ -29,21 +29,21 @@ function health.check()
 end
 
 function health.report_views()
-  vim.health.report_start('view::active')
+  vim.health.start('view::active')
 
   local view_roots = vim.tbl_map(function(item)
     return item.root
-  end, view.items)
+  end, view.get_sorted_items())
 
   table.sort(view_roots)
 
   for _, root in ipairs(view_roots) do
-    vim.health.report_info(root.path)
+    vim.health.info(root.path)
   end
 end
 
 function health.report_events()
-  vim.health.report_start('watcher::events')
+  vim.health.start('watcher::events')
 
   local names = vim.tbl_keys(watcher.events)
 
@@ -51,7 +51,7 @@ function health.report_events()
 
   for _, name in ipairs(names) do
     local callback_count = #vim.tbl_keys(watcher.events[name] or {})
-    local reporter = callback_count == 0 and 'report_warn' or 'report_info'
+    local reporter = callback_count == 0 and 'warn' or 'info'
 
     vim.health[reporter](
       string.format(
@@ -65,14 +65,14 @@ function health.report_events()
 end
 
 function health.report_listeners()
-  vim.health.report_start('watcher::listeners')
+  vim.health.start('watcher::listeners')
 
   local paths = vim.tbl_keys(watcher.listeners)
 
   table.sort(paths, sort_paths)
 
   for _, path in ipairs(paths) do
-    vim.health.report_info(path)
+    vim.health.info(path)
   end
 end
 
